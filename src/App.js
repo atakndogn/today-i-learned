30aaad862ce008931f9f492efd20b6cdf455bb76
Atakan <atakan.dogan@gmail.com>
2023-02-15 05:53:16 +0300

    state and Header Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
+import { useState } from "react";
 import "./style.css";
-
-const CATEGORIES = [
-  { name: "technology", color: "#3b82f6" },
-  { name: "science", color: "#16a34a" },
-  { name: "finance", color: "#ef4444" },
-  { name: "society", color: "#eab308" },
-  { name: "entertainment", color: "#db2777" },
-  { name: "health", color: "#14b8a6" },
-  { name: "history", color: "#f97316" },
-  { name: "news", color: "#8b5cf6" },
-];
 
 // supbase pwd: juFP1pAeNjOYrlSF
 
@@ -48,18 +38,12 @@ const initialFacts = [
 ];
 
 function App() {
+  const [isShowForm, setIsShowForm] = useState(false);
+
   return (
     <>
-      <header className="header">
-        <div className="logo">
-          <img src="logo.png" alt="Today I Learned logo" />
-          <h1>Today I Learned</h1>
-        </div>
-        <button className="btn btn-large btn-open">Share a fact</button>
-      </header>
-
-      <NewFactForm />
-
+      <Header isShowForm={isShowForm} setIsShowForm={setIsShowForm} />
+      {isShowForm && <NewFactForm />}
       <main className="main">
         <CategoryFilter />
         <FactList />
@@ -68,12 +52,58 @@ function App() {
   );
 }
 
+function Header({ isShowForm, setIsShowForm }) {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" alt="Today I Learned logo" />
+        <h1>Today I Learned</h1>
+      </div>
+      <button
+        className="btn btn-large btn-open"
+        onClick={() => setIsShowForm((s) => (s = !s))}
+      >
+        {isShowForm ? "Close" : "Share a fact"}
+      </button>
+    </header>
+  );
+}
+
 function NewFactForm() {
   return <form className="fact-form">Fact Form</form>;
 }
 
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
 function CategoryFilter() {
-  return <aside>Category Filter</aside>;
+  return (
+    <aside>
+      <ul>
+        <li className="category">
+          <button className="btn btn-all-categories">All</button>
+        </li>
+        {CATEGORIES.map((c) => (
+          <li key={c.name} className="category">
+            <button
+              className="btn btn-category"
+              style={{ backgroundColor: c.color }}
+            >
+              {c.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 function FactList() {
@@ -90,7 +120,6 @@ function FactList() {
 }
 
 function Fact({ factObj }) {
-  console.log(factObj.id);
   return (
     <li className="fact">
       <p>
