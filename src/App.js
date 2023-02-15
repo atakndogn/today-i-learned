bf465d846a56c6dd052d3de024d126abe2d10318
Atakan <atakan.dogan@gmail.com>
2023-02-15 07:46:41 +0300

    Adding a new fact was complted

--- src/App.js
+++ src/App.js
@@ -39,14 +39,17 @@ const initialFacts = [
 
 function App() {
   const [isShowForm, setIsShowForm] = useState(false);
+  const [facts, setFacts] = useState(initialFacts);
 
   return (
     <>
       <Header isShowForm={isShowForm} setIsShowForm={setIsShowForm} />
-      {isShowForm && <NewFactForm />}
+      {isShowForm && (
+        <NewFactForm setIsShowForm={setIsShowForm} setFacts={setFacts} />
+      )}
       <main className="main">
         <CategoryFilter />
-        <FactList />
+        <FactList facts={facts} />
       </main>
     </>
   );
@@ -80,21 +83,57 @@ const CATEGORIES = [
   { name: "news", color: "#8b5cf6" },
 ];
 
-function NewFactForm() {
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+function NewFactForm({ setIsShowForm, setFacts }) {
   const [text, setText] = useState("");
-  const [source, setSource] = useState("");
+  const [source, setSource] = useState("http://example.com");
   const [category, setCategory] = useState("");
+
+  function handleSubmit(e) {
+    // 1. Prevent browser reload
+    e.preventDefault();
+
+    // 2. Check if data is valid. If so, create a new fact
+    if (text && text.length <= 200 && isValidHttpUrl(source) && category) {
+      // 3. Create a new fact object
+      const newFact = {
+        id: Math.round(Math.random() * 10000000000),
+        text,
+        source,
+        category,
+        votesInteresting: 0,
+        votesMindblowing: 0,
+        votesFalse: 0,
+        createdIn: new Date().getFullYear(),
+      };
+
+      // 4. Add the new fact to the UI: add the fact to state
+      setFacts((facts) => [newFact, ...facts]);
+
+      // 5. Reset input fields
+      setText("");
+      setSource();
+      setCategory();
+
+      // 6. Close the form
+      setIsShowForm(false);
+    }
+  }
 
   const categoryList = CATEGORIES.map((cat) => (
     <option key={cat.name} value={cat.name}>
       {cat.name.toUpperCase()}
     </option>
   ));
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(text, source, category);
-  }
 
   return (
     <form className="fact-form" onSubmit={handleSubmit}>
@@ -142,9 +181,7 @@ function CategoryFilter() {
   );
 }
 
-function FactList() {
-  const facts = initialFacts;
-
+function FactList({ facts }) {
   const factsList = facts.map((fact) => <Fact key={fact.id} factObj={fact} />);
 
   return (
